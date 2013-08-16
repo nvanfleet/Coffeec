ffa48e342356bd037724f0fad721f576decf8d78
Nathan Van Fleet <nathan.vanfleet@gmail.com>
2013-08-16 10:57:17 -0400

    changed files

--- src/coffeec.c
+++ src/coffeec.c
@@ -1,3 +1,9 @@
+// Copyright (C) 2013 Nathan Van Fleet
+//
+// This is free software, licensed under the GNU General Public License v2.
+// See /LICENSE for more information.
+//
+
 /* ------------------------------------------------------------------------- */
 /*       coffeedc -- A PID system for espresso machines                       */
 /* ------------------------------------------------------------------------- */
